fc9f2ba75ac26362bce46dc1b33b08f08d31551e
tourinhoguilherme <85791380+tourinhoguilherme@users.noreply.github.com>
2021-06-24 22:17:04 -0300

    Add files via upload
    Entrega pós aula 3 - HTML + CSS + JAVASCRIPT + USO DE FRAMEWORKS

--- main.js
+++ main.js
@@ -90,3 +90,56 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+//SEÇÃO DE DEPOIMENTOS COM CARROSSEL/SLIDER
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* ESTE PEDAÇO FOI COPIADO DO SWIPER, MAS SUBSTITUÍDO PELA ESTRUTURA ACIMA.
+{
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+}); */
+
+//FUNCIONALIDADE SCROLLREVEAL
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 750,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links`,
+  {
+    interval: 100
+  }
+)
